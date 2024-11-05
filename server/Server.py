b6e0f6951c6bc788805a4f9f2848e61a7004e627
Porvah <3msabdullah@gmail.com>
2024-11-06 01:48:38 +0200

    added performance + edited server thread count

--- server/Server.py
+++ server/Server.py
@@ -1,14 +1,22 @@
 import socket
 import threading
 from handlers import GET_handler, get_content_length, POST_handler
-
-
+defTimout = 20.0
+threadsCount = 1
+count_lock = threading.Lock()
+max_thread_count = 1
 def handle_client(client_socket, addr):
+    global threadsCount
+    global max_thread_count
+    with count_lock:  # Ensure exclusive access
+        threadsCount += 1
+        max_thread_count = max(max_thread_count, threadsCount)
     try:
         while True:
             # receive and print client messages
             request = client_socket.recv(1024)
             if request:
+                client_socket.settimeout(defTimout/threadsCount)
                 request2 = request.decode('utf-8', errors='ignore')
                 command = request2.splitlines()[0]
                 method, file_path, _ = command.split()
@@ -28,11 +36,17 @@ def handle_client(client_socket, addr):
                 client_socket.send(response)
             else:
                 break
+    except socket.timeout:
+        print("connection timedout")
     except Exception as e:
         print(f"Error when hanlding client: {e}")
     finally:
         client_socket.close()
+        with count_lock:
+            threadsCount-= 1
         print(f"Connection to client ({addr[0]}:{addr[1]}) closed")
+        print("maximum concurrent threads = "+ str(max_thread_count-1))
+
 
 
 def run_server():
@@ -46,10 +60,11 @@ def run_server():
         # listen for incoming connections
         server.listen()
         print(f"Listening on {server_ip}:{port}")
-
+        
         while True:
             # accept a client connection
             client_socket, addr = server.accept()
+            client_socket.settimeout(defTimout/threadsCount)
             print(f"Accepted connection from {addr[0]}:{addr[1]}")
             # start a new thread to handle the client
             thread = threading.Thread(target=handle_client, args=(client_socket, addr,))
